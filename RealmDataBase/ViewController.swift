108849c2f6c379135ec61efdc1d4a330d25f9d6f
media-pt <media-pt@mac-mini-media-pt.local>
2016-11-04 15:51:08 +0200

    Initial Commit

---
+++ RealmDataBase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmDataBase
+//
+//  Created by media-pt on 04.11.16.
+//  Copyright © 2016 media-pt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
